4e1ae9f918afaa2f0bac80e8b618d47720d0951d
charles <952046097@qq.com>
2019-07-05 12:04:11 +0800

    feat: 164. Maximum Gap

---
+++ test/array/164. Maximum Gap.test.js
@@ -0,0 +1,9 @@
+import maximumGap from '../../code/array/164. Maximum Gap'
+
+test('sort:input2', () => {
+	expect(maximumGap([3, 6, 9, 1])).toBe(3)
+})
+
+test('sort:input3', () => {
+	expect(maximumGap([10])).toBe(0)
+})
