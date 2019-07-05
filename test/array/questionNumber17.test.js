57933d524253db68baf7114e92abdffaf9585bb5
charles <952046097@qq.com>
2019-07-05 12:04:16 +0800

    remove

--- test/array/questionNumber17.test.js
+++
@@ -1,5 +0,0 @@
-import questionNumber17 from '../../code/array/questionNumber17'
-
-test('revertByWorld:Let\'s take LeetCode contest', () => {
-	// expect(questionNumber17("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
-})
